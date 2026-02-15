909470f41c8d8ce3c7319d99e2259ebe57c4a9f2
jayadurga-t <jayadurga.t332@gmail.com>
2026-02-15 23:28:00 +0530

    loans: migrate persistence layer from H2 to MySQL and rectified error regarding createdAt field in Base Entity

--- loans/src/main/java/com/avo/loans/entity/BaseEntity.java
+++ loans/src/main/java/com/avo/loans/entity/BaseEntity.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -24,7 +25,7 @@ public class BaseEntity {
     @Column(updatable = false)
     private String createdBy;
 
-    @CreatedBy
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
