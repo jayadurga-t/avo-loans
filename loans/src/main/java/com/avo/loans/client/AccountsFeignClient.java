0f6549547f5c153552ccad8a0f43df1f1136f67e
jayadurga-t <jayadurga.t332@gmail.com>
2026-02-21 15:56:59 +0530

    loans: integrate OpenFeign client for inter-service communication and back to h2 from mysql

---
+++ loans/src/main/java/com/avo/loans/client/AccountsFeignClient.java
@@ -0,0 +1,15 @@
+package com.avo.loans.client;
+
+import com.avo.loans.dto.CustomerDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("accounts")
+public interface AccountsFeignClient {
+
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber);
+
+}
---
+++ loans/src/main/java/com/avo/loans/dto/CustomerDto.java
@@ -0,0 +1,49 @@
+package com.avo.loans.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer information"
+)
+public class CustomerDto {
+
+    @Schema(
+            description = "Name of the Customer",
+            example = "Avo"
+    )
+    @NotEmpty(message = "Name cannot be null or empty")
+    @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
+    private String name;
+
+    @Schema(
+            description = "Email address of the Customer",
+            example = "customer@avo.com"
+    )
+    @NotEmpty(message = "Email cannot be null or empty")
+    @Email(message = "Email address should be a valid value")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@gmail\\.com$", message = "Email address must be a valid @gmail.com email")
+    private String email;
+
+    @Schema(
+            description = "Mobile number of Customer",
+            example = "9876543210"
+    )
+    @NotEmpty(message = "Mobile number cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    private String mobileNumber;
+
+    @Schema(
+            description = "Account details of the Customer"
+    )
+    @Valid
+    private AccountsDto accountsDto;
+
+}
